ae814b9c6fdeab993d3f1b5019de4c1ba28a32c0
owen <owen.chosen@gmail.com>
2023-08-02 09:18:36 +0100

    Done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+
+function findMatching (driversNames, name) {
+    return driversNames.filter ((currentName) => {
+        if (currentName.toUpperCase() === name.toUpperCase()) {
+            return currentName; } 
+    });
+}
+
+function fuzzyMatch (driversNames, name) {
+    return driversNames.filter ((currentName) => {
+        let isComprise = false;
+        for (let i = 0; i < name.length; i++ ) {
+            if (name.charAt(i).toUpperCase() == currentName.charAt(i).toUpperCase()) isComprise = true;
+        }
+        if (isComprise) return currentName;
+    });
+}
+
+function matchName(drivers, name) {
+    return drivers.filter ((driver) => {
+        if (driver.name.toUpperCase() === name.toUpperCase()) return driver;
+    });
+}
